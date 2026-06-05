c853d35fbe10deefec12090c594c2180660c8abc
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-06-05 18:56:24 +0530

    initial commit — frontend + backend

---
+++ devlens-backend/src/index.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config({ path: path.join(__dirname, '.env') })
+
+const app = require('./app')
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
