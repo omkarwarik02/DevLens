6c1cfb66e8428b7b901a6af4de6ebd76fdb5c93b
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-06-05 22:15:21 +0530

    Created review code route and registred it and fixed protect middleware import

--- devlens-backend/src/app.js
+++ devlens-backend/src/app.js
@@ -3,12 +3,14 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
+const reviewRoutes = require("./routes/reviewRoutes")
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', authRoutes);
+app.use('/api/review',reviewRoutes)
 
 mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB connected')).catch((err)=>console.log('MongoDB error:', err))
 
--- devlens-backend/src/controllers/reviewController.js
+++ devlens-backend/src/controllers/reviewController.js
@@ -1,32 +1,39 @@
-const Groq = require('groq-sdk')
+const Groq = require("groq-sdk");
 const Review = require("../models/Review");
 
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
+const reviewCode = async (req, res) => {
+  try {
+    const { code, language } = req.body;
 
-const groq = new Groq({apiKey:process.env.GROQ_API_KEY});
-
-const reviewCode = async(req,res) => {
-    const {code,language} = req.body;
+    if (!code || !language) {
+      return res
+        .status(400)
+        .json({ message: "Code and language are required" });
+    }
 
     const response = await groq.chat.completions.create({
-        model:"llama-3.3-70b-versatile",
-        messages:[
-            {
-                role:"user",
-                content:`Review this ${language} code:${code}`
-            }
-        ]
-    })
-    const aiReview = response.choices[0]?.message?.content || ""
+      model: "llama-3.3-70b-versatile",
+      messages: [
+        {
+          role: "user",
+          content: `Review this ${language} code:${code}`,
+        },
+      ],
+    });
+    const aiReview = response.choices[0]?.message?.content || "";
 
     await Review.create({
-        userId:req.user.id,
-        code,
-        language,
-        aiReview
-    })
-    res.status(200).json({aiReview})
-}
+      userId: req.user.id,
+      code,
+      language,
+      aiReview,
+    });
+    res.status(200).json({ aiReview });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
-
-module.exports = { reviewCode }
+module.exports = { reviewCode };
