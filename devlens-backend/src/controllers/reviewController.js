a6cfa24484050296d383e7755e28ce7957522bdc
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-06-05 20:58:23 +0530

    Created review controller

---
+++ devlens-backend/src/controllers/reviewController.js
@@ -0,0 +1,32 @@
+const Groq = require('groq-sdk')
+const Review = require("../models/Review");
+
+
+
+const groq = new Groq({apiKey:process.env.GROQ_API_KEY});
+
+const reviewCode = async(req,res) => {
+    const {code,language} = req.body;
+
+    const response = await groq.chat.completions.create({
+        model:"llama-3.3-70b-versatile",
+        messages:[
+            {
+                role:"user",
+                content:`Review this ${language} code:${code}`
+            }
+        ]
+    })
+    const aiReview = response.choices[0]?.message?.content || ""
+
+    await Review.create({
+        userId:req.user.id,
+        code,
+        language,
+        aiReview
+    })
+    res.status(200).json({aiReview})
+}
+
+
+module.exports = { reviewCode }
