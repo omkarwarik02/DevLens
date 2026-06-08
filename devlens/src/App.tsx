f219257ddd5f5347099597fdb17902fc22883385
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-06-08 22:47:12 +0530

    Creating dashboard done with left panel

--- devlens/src/App.tsx
+++ devlens/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   const location = useLocation()
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Landing></Landing>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="/dashboard" element={ <Dashboard></Dashboard>}></Route>
       </Routes>
     </div>
   )
--- devlens/src/pages/Dashboard.tsx
+++ devlens/src/pages/Dashboard.tsx
@@ -1,5 +1,36 @@
 function Dashboard() {
-  return <div>Landing</div>
+  return (
+    <div className="min-h-screen bg-[#0d1117] p-6">
+      <div className="flex gap-6 mt-20">
+        {/**left panel */}
+        <div className="w-1/2 bg-[#161b22] rounded-xl p-6">
+          <h1 className="font-bold text-2xl">Code Review</h1>
+          <div>
+            <h3 className="mt-3 text-[11px]">PROGRAMMING LANGUAGE</h3>
+
+            <select className="h-[46px] w-[554px] bg-[#0D1117] p-2 mt-3">
+              <option value="javascript">JavaScript</option>
+              <option value="typescript">TypeScript</option>
+              <option value="java">Java</option>
+              <option value="c++">C++</option>
+              <option value="c">C</option>
+              <option value="python">Python</option>
+            </select>
+            <div>
+              <h3 className="text-[11px] mt-3">SOURCE CODE</h3>
+              <textarea
+                className="w-full h-64 bg-[#0d1117] text-white font-mono text-sm p-3 rounded-lg border border-gray-700 resize-none outline-none mt-3"
+                placeholder="Paste your code here..."
+              />
+            </div>
+          </div>
+        </div>
+
+        {/**right panel */}
+        <div className="w-1/2 bg-[#161b22] rounded-xl p-6"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
