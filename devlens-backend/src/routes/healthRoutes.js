9ffc8139fc694d6e93d6d1959e8b28f3c337dfc4
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-07-09 19:44:35 +0530

    Fixed health route

--- devlens-backend/src/routes/healthRoutes.js
+++ devlens-backend/src/routes/healthRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 
 
-app.get('/health',(req,res)=>{
+router.get('/health',(req,res)=>{
     res.status(200).json({status:'ok',time:new Date().toISOString()});
 });
 
